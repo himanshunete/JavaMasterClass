91ed2d5b53ab7d2824b5f05a2484e2ce619d4852
himanshunete <himanshuneteh@gmail.com>
2021-01-08 18:08:37 +0530

    ADDED: functionality to perform addition

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,10 @@
 public class HelloJava {
-  public static void main(String[] args) {
+     public static void main(String[] args) {
      System.out.println("Hello Java"); 
+     System.out.println(add(2,3));
+
+     }
+     public static int add(int b,int d ){  
+        return b + d;
   }
 }
