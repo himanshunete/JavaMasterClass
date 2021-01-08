73a9460f3f80ba780cdbc45d1b5e9bfb5eb0d963
himanshunete <himanshuneteh@gmail.com>
2021-01-08 18:26:45 +0530

    ADDED: functionality to perform subtraction

--- HelloJava.java
+++ HelloJava.java
@@ -1,10 +1,13 @@
 public class HelloJava {
      public static void main(String[] args) {
      System.out.println("Hello Java"); 
-     System.out.println(add(2,3));
-
+     System.out.println(add(3,2));
+     System.out.println(sub(3,2));
      }
      public static int add(int b,int d ){  
         return b + d;
   }
+     public static int sub(int x,int y ){
+        return x - y;
+     }
 }
